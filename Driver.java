874693b684c9ac3fda5fb29052cf597cb9ba53b2
jlin03 <jlin03@stuy.edu>
2019-03-22 14:49:04 -0400

    driver

--- Driver.java
+++ Driver.java
@@ -1,7 +1,35 @@
+import java.util.*;
 public class Driver {
   public static void main(String[] args) {
     MyDeque<Integer> test = new MyDeque<Integer>();
+    test.addFirst(1);
+    System.out.println(Arrays.toString(test.data));
     System.out.println(test);
+    System.out.println(test.size());
+    test.addFirst(2);
+    System.out.println(Arrays.toString(test.data));
+    System.out.println(test);
+    System.out.println(test.size());
+    test.addFirst(3);
+    System.out.println(Arrays.toString(test.data));
+    System.out.println(test);
+    System.out.println(test.size());
+    test.addFirst(4);
+    System.out.println(Arrays.toString(test.data));
+    System.out.println(test);
+    System.out.println(test.size());
+    test.addFirst(5);
+    System.out.println(Arrays.toString(test.data));
+    System.out.println(test);
+    System.out.println(test.size());
+    test.addFirst(6);
+    System.out.println(Arrays.toString(test.data));
+    System.out.println(test);
+    System.out.println(test.size());
+    test.addFirst(7);
+    System.out.println(Arrays.toString(test.data));
+    System.out.println(test);
+    System.out.println(test.size());
 
 
   }
